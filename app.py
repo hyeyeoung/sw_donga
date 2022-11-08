5b70adc1922a35b7248247e7963d6a5a9ec4dc2d
hy <rlagpdud0524@gmail.com>
2022-11-09 01:15:18 +0900

    개발 준비

--- app.py
+++ app.py
@@ -4,11 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return redirect(url_for('home'))
+    return redirect(url_for('input'))
 
-@app.route('/home')
-def home():
-    return render_template('test.html')
+@app.route('/input')
+def input():
+    return render_template('input.html')
+
+@app.route('/input_back', methods = ['POST'])
+def input_back():
+    hello = request.form['input']  # 요청한 값을 리스트 형태로 hello에 저장
+    function(hello)   # 함수 사용
+    return render_template('output.html', hello = hello)  # hello는 데이터값 동적 변수로 html에서 사용 가능(단, 플라스크는 동작해야함)
+
+# ------------------------------------------
+# 일반 함수(프로그램을 돌리면서 필요한 일반 함수들)
+def function(name):
+    print(name)
 
 if __name__ == '__main__':
     app.debug = True
