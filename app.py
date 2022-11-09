805e5045641716cda55c2ea6746b26a01e07199b
meanjoo <meanjooo@naver.com>
2022-11-09 17:04:20 +0900

    입력

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 from flask import Flask
 from flask import Flask, render_template, request, session, redirect, url_for
+# render_template 함수는 flask에서 제공하는 함수로 templates에 저장된 html을 불러올 때 사용하는 함수이다.
+
 app = Flask(__name__)
 
+
+# flask에서는 decorator가 URL 연결에 사용된다. 다음 행의 함수부터 decorator가 적용된다.
 @app.route('/')
 def hello_world():
     return redirect(url_for('input'))
 
-@app.route('/input')
-def input():
-    return render_template('input.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-@app.route('/input_back', methods = ['POST'])
-def input_back():
-    hello = request.form['input']  # 요청한 값을 리스트 형태로 hello에 저장
-    function(hello)   # 함수 사용
-    return render_template('output.html', hello = hello)  # hello는 데이터값 동적 변수로 html에서 사용 가능(단, 플라스크는 동작해야함)
-
-# ------------------------------------------
-# 일반 함수(프로그램을 돌리면서 필요한 일반 함수들)
-def function(name):
-    print(name)
+@app.route('/output', methods=['GET', 'POST'])
+def output():
+    if request.method == 'POST':
+        value = request.form['code']
+        print(value)
+    return render_template('output.html', value=value)
 
 if __name__ == '__main__':
     app.debug = True
+    # debug=True 없이 기본으로 실행하면 수정 사항을 반영하기 위해 서버를 재기동해야 한다.
     app.run(debug=True)
